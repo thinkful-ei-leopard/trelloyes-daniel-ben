677a10796a0cc8cae10d2cab53a031d0f8580959
Daniel Fong <daniel.fong.mv@gmail.com>
2020-01-27 15:34:10 -0700

    do we get it yet?

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import STORE from './store';
+import Card from './Card';
+import List from './List';
 
-function App() {
+class App extends Component {
+    static defaultProps = {
+      store: {
+        lists: [],
+        allCards: {},
+      }
+    };
   
-}
-
-export default App;
+    render() {
+      const { store } = this.props
+      return (
+        <main className='App'>
+          <header className='App-header'>
+            <h1>Trelloyes!</h1>
+          </header>
+          <div className='App-list'>
+            {store.lists.map(list => (
+              <List
+                key={list.id}
+                header={list.header}
+                cards={list.cardIds.map(id => store.allCards[id])}
+              />
+            ))}
+          </div>
+        </main>
+      );
+    }
+  }
+  
+  export default App;
