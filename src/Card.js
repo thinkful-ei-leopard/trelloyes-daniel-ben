76ca8bb94ec7a5e2f3d5f45d36e357ed09de71b6
Daniel Fong <daniel.fong.mv@gmail.com>
2020-01-27 13:58:03 -0700

    finished Card.js maybe

---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import React from '.react'
+import list from './list'
+import { render } from "@testing-library/react"
+
+class cardClass extends React.Component {
+    render() {
+        return (
+        <div class="Card">
+            <button type="button">delete</button>
+            <h3>{this.props.title}</h3>
+            <p>{this.props.content}</p>
+        </div>
+        )
+    }
+}
+
+export default cardClass;
+    
